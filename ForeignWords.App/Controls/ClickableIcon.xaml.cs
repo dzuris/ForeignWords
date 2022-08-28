488cdebed5c2adba4afb028af8d83eca19a8370e
Adam Dzurilla <xdzuri00@vutbr.cz>
2022-08-28 11:00:12 +0200

    Styles Combo box, button

---
+++ ForeignWords.App/Controls/ClickableIcon.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ForeignWords.App.Controls
+{
+    /// <summary>
+    /// Interaction logic for ClickableIcon.xaml
+    /// </summary>
+    public partial class ClickableIcon : UserControl
+    {
+        public ICommand Command
+        {
+            get => (ICommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+
+        public string IconSource
+        {
+            get => (string)GetValue(IconSourceProperty);
+            set => SetValue(IconSourceProperty, value);
+        }
+
+        public static readonly DependencyProperty CommandProperty = DependencyProperty.Register(
+            nameof(Command), typeof(ICommand), typeof(ClickableIcon));
+
+        public static readonly DependencyProperty IconSourceProperty = DependencyProperty.Register(
+            nameof(IconSource), typeof(string), typeof(ClickableIcon));
+
+        public ClickableIcon()
+        {
+            InitializeComponent();
+        }
+    }
+}
